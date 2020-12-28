a40f2925f169047851cf005d6a5fcdac709b4c8b
myfanwybr <m.brownrobinson@hotmail.com>
2020-12-28 14:41:37 -0500

    commit

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,14 +1,24 @@
-from flask import Flask
-from flask_pymongo import pymongo
+from flask import Flask, jsonify, render_template, redirect
+from flask_pymongo import PyMongo
+import mars_scrape
+
 
 app=Flask(__name__)
 
-conn='mongodb://localhost:27017'
-client=pymongo.MongoClient(conn)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
 
 @app.route("/")
-def main():
-        return("hello world!")
+def home():
+        space_data=mongo.db.collection.find_one()
+
+        return render_template("index.html", mars=space_data)
+
+@app.route("/scrape")
+def scrape():
+        mars_data=mars_scrape.scrape_info()
+        mongo.db.collection.update({}, mars_data, upsert=True)
+        return redirect("/")
 
 if __name__=='__main__':
     app.run(debug=True)
